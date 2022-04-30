2e47d5bfc8a84fd3d6df9fb6321bd41ff5bbadd5
wanlijun <wanlijun_cq@126.com>
2022-04-30 20:55:11 +0800

    ssh test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 // TODO如何配置了消除这句
 import React from 'react';
-import { Hello } from './common';
-const App = () => {
-  Hello()
+import _ from './common';
+const App =  () => {
+  import('./common')
+  .then((obj) => {
+    console.log(obj)
+  });
+  console.log(obj)
   return <div>Apph</div>
 }
 export default App
